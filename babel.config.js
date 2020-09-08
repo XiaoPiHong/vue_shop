7cd46c8296f2901789453e82de478922038cc4b3
zzh <260094892@qq.com>
2020-09-08 19:30:33 +0800

    优化-只在发布阶段移除所有的console

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,17 @@
 /*
  * @Author: your name
  * @Date: 2020-08-28 16:25:40
- * @LastEditTime: 2020-09-08 14:31:00
+ * @LastEditTime: 2020-09-08 14:44:09
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue_shop\babel.config.js
  */
+// 项目发布阶段需要用到的babel插件
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
@@ -18,6 +24,7 @@ module.exports = {
         styleLibraryName: 'theme-chalk'
       }
     ],
-    'transform-remove-console'
+    // 发布产品时候的插件数组
+    ...prodPlugins
   ]
 }
